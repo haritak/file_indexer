15a94df892c598e775a028d9e4e3675b76e62e2c
haritak <git@ych.gr>
2016-05-05 06:33:42 +0000

    Going to multi-threaded

--- dirlisting.rb
+++ dirlisting.rb
@@ -19,70 +19,86 @@ begin
 	recorder = Recorder.new
 	puts "Initialized"
 
-	#recorder.checkFilenames
-	#exit
-
 	@start = Time.now
+	@counter = 0
+
 
 	Dir.glob("**/*").each_with_index do |fn,i|
-	  if not File.directory?(fn)
+	  @counter+=1
+	  if @counter== 1 
+		puts "Started"
+	  end
 
-		begin
-			ap = File.realpath(fn)
-		rescue => e
-			log.log_skipped(fn, "File.realpath fails, probably broken link")
-			next
+	  while Thread.list.count > 50 do
+		puts "waiting for some threads to finish"
+		while Thread.list.count > 25 do
+			puts Thread.list.count
+			sleep 1
+			puts Thread.list.count
+			sleep 1
 		end
-		if not File.readable?(ap) 
-			log.log_skipped(ap, "not readable")
-			next
-		end
+	  end
+	  Thread.new do
+ 		  local_fn = fn
+		  if not File.directory?(local_fn)
 
-	        puts "----Processing file number #{i} ----"
-		p ap
-		puts ap
-
-		sz = File.size(ap);
-		mt = File.mtime(ap).to_f;
-
-	        puts "#{sz}", "#{mt}"
-
-		if recorder.exists_file?(ap)  
-			p_sz = recorder.getSize(ap).to_i
-			p_mt = recorder.getModTime(ap).to_f
-			
-			puts "File was found with size #{p_sz} and modification time #{p_mt}"
-			
-			if p_sz == sz && p_mt == mt
-				log.log_skipped(ap, "same size, same mt already recorded")
-				puts "same size, same mod time, skipped"
-			else
-				sha = calcSha(ap)
-				puts "Record updated due to file contents change"
-				recorder.updateFile(ap, sha, sz, mt)
+			begin
+				ap = File.realpath(local_fn)
+			rescue => e
+				log.log_skipped(local_fn, "File.realpath fails, probably broken link")
+				Thread.exit
 			end
-		else
-			sha=calcSha(ap) 
+			if not File.readable?(ap) 
+				log.log_skipped(ap, "not readable")
+				Thread.exit
+			end
 
-			if recorder.exists_sha?(sha) 
-				previous_ap = recorder.getFullpath(sha);
+			puts "----Processing file number #{i} ----"
+			p ap
+			puts ap
 
-				if File.identical?(ap, previous_ap)
-					log.log_skipped(ap, "Identical as #{previous_ap}")
-					puts "Is identical to #{previous_ap}"
-			
+			sz = File.size(ap);
+			mt = File.mtime(ap).to_f;
+
+			puts "#{sz}", "#{mt}"
+
+			if recorder.exists_file?(ap)  
+				p_sz = recorder.getSize(ap).to_i
+				p_mt = recorder.getModTime(ap).to_f
+				
+				puts "File was found with size #{p_sz} and modification time #{p_mt}"
+				
+				if p_sz == sz && p_mt == mt
+					log.log_skipped(ap, "same size, same mt already recorded")
+					puts "same size, same mod time, skipped"
 				else
-					puts "Recorded as duplicate to #{previous_ap}"
-					recorder.insertAsDuplicate(sha, ap)
+					sha = calcSha(ap)
+					puts "Record updated due to file contents change"
+					recorder.updateFile(ap, sha, sz, mt)
 				end
 			else
-				puts "Recorded as brand new"
-				recorder.insertToDb(sha, ap, sz, mt) 
+				sha=calcSha(ap) 
+
+				if recorder.exists_sha?(sha) 
+					previous_ap = recorder.getFullpath(sha);
+
+					if File.identical?(ap, previous_ap)
+						log.log_skipped(ap, "Identical as #{previous_ap}")
+						puts "Is identical to #{previous_ap}"
+				
+					else
+						puts "Recorded as duplicate to #{previous_ap}"
+						recorder.insertAsDuplicate(sha, ap)
+					end
+				else
+					puts "Recorded as brand new"
+					recorder.insertToDb(sha, ap, sz, mt) 
+				end
 			end
 		end
 
 		puts ""
-
+		Thread.exit
 	  end
 
 	end
