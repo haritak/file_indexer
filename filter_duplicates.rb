909f96f404aea5acf743c760e404305b5428f43a
haritak <git@ych.gr>
2016-05-15 01:01:09 +0000

    filtering updated complete, based upon sequel

--- filter_duplicates.rb
+++ filter_duplicates.rb
@@ -1,139 +1,101 @@
 #!/usr/bin/ruby
-
+#require 'rubygems'
 require 'mysql'
-require 'time'
-require 'shellwords'
+require 'sequel'
+require 'logger'
 require 'thread'
+require 'digest'
 load '/home/yannos/scripts/credentials.txt'
 
-class Duplicates
-	include Enumerable
+Thread::abort_on_exception=true
 
-	def initialize
-		@con = Mysql.new 'localhost', MY_USERNAME, MY_PASSWORD, 'yannos'
-		@rs = @con.query("SELECT sha256,fullpath FROM duplicate_files")
-		puts @rs.num_rows
-	end
+DB = Sequel.connect("mysql://#{MY_USERNAME}:#{MY_PASSWORD}@localhost/yannos")
+L = Logger.new($stdout)
+DB.logger = L
 
-	def each
-		while row = @rs.fetch_row do 
-			yield row
-		end
-	end
+_Files = DB[:_files]
+_Dups = DB[:_duplicates]
 
-	def close
-		@con.close if @con
-	end
+@counter = 0;
+@start = Time.now
+semaphore = Mutex.new
+_Dups.select(:sha, :apath, :bytes, :seconds).all do |row|
 
-
-	def find_original(sha)
-                rs = @con.query ("SELECT fullpath FROM files WHERE sha256=\"#{sha}\";")
-                raise "Unpredictable error" if rs.num_rows > 1
-		rs.num_rows == 1 ? rs.fetch_row[0] : nil;
-        end
-end
-
-def calcSha(ap)
-	cmd = "sha256sum \"#{ap}\""
-	s = `#{cmd}`
-	s.length<64 ? nil : s[0..63]
-end
-
-class Logger
-        def initialize(filename)
-                @log = File.new(filename, "a")
-                @log.puts("--- new run ---")
-        end
-
-        def log(f)
-                @log.puts(f)
-        end
-
-        def close
-                @log.close if @log
-        end
-end
-
-begin
-
-	dups = Duplicates.new
-	log = Logger.new("filter_duplicates.log")
-	failed = Logger.new("filter_duplicates.failed.log")
-
-	@counter = 0;
-
-	@start = Time.now
-
-	semaphore = Mutex.new
-
-	dups.each do |row|
-
-		sleep 10 while Thread.list.count > 50
-
-		Thread.new do
-		begin
-			sha = ap = o_ap = ''
-			semaphore.synchronize {
-				sha = row[0]
-				ap =  row[1]
-				o_ap = dups.find_original(sha)
-			}
-
-			if ap==o_ap
-				print "="
-				Thread.exit
-			elseif File.identical?(ap, o_ap) 
-				print "."
-				Thread.exit
-			elseif  not File.exists?(ap) or not File.exists?(o_ap)
-				print "x"
-				Thread.exit 
-			else 
-				new_sha = calcSha( ap )
-				if (new_sha!= sha )
-					 raise "Shas missmatch!"
-				end
-				new_sha = calcSha( o_ap )
-				if (new_sha!= sha )
-					 raise "Shas missmatch 2!"
-				end
-				if (File.size(ap) != File.size(o_ap)) 
-					raise "Files with the same sha, but different size!"
-				end
-				
-				if File.writable?(ap)
-					semaphore.synchronize {
-						log.log("Unlinking #{ap}, Linking with #{o_ap}")
-						@counter+=1
-					}
-					#File.unlink(ap)
-					#File.link(o_ap, ap)
-					print "+"
-				else
-					puts ap
-					exit
-					print "!"
-					semaphore.synchronize {
-						failed.log("Not writable : #{ap}")
-					}
-				end
-			end
-		end
-
-		end #thread
-	end
-
-rescue Mysql::Error => e
-    puts e.errno
-    puts e.error
-
-ensure
-	dups.close if dups
-	log.close if log
-	failed.close if failed
-
-	elapsed = Time.now - @start
+	sha = ap = sz = mt = ""
+	semaphore.synchronize {
+		sha = row[:sha]
+		ap =  row[:apath]
+		sz = row[:bytes]
+		mt = row[:seconds]
+	}
 	
-	puts "#{@counter} files were linked"
-	puts "#{elapsed} seconds"
-end
+	Thread.new do
+
+		mysha = myap = mysz = mymt = " "
+		semaphore.synchronize {
+			mysha = sha
+			myap = ap
+			mysz = sz
+			mymt = mt
+		}
+
+		#get the original file
+		o = _Files.select(:sha, :apath, :bytes, :seconds).first(:sha=>mysha)
+		if not o 
+			L.warn("sha not found")
+			Thread.exit
+		end
+		
+		oap = o[:apath]
+
+		L.info("Working on #{myap} which is similar in content to #{oap}")
+
+		if oap == myap
+			L.info("They are the same")
+			Thread.exit
+		end
+
+		if not File.exists?(oap)
+			L.info("Original file has been deleted")
+			Thread.exit
+		end
+
+		if not File.exists?(myap)
+			L.info("Duplicate file has been deleted")
+			Thread.exit
+		end
+
+
+		if File.identical?(oap, myap)
+			L.warn("Files are identical")
+			Thread.exit
+		end
+
+		osz = File.size(oap)
+		omt = File.modtime(oap)
+		
+
+		if File.size(oap) == mysz &&
+			File.modtime(oap) == mymt &&
+			File.size(myap) == mysz &&
+			File.modtime(myap) == mymt	
+
+			L.info("Unlinking #{myap}"
+			File.unlink(myap)
+			File.link(oap, myap)
+			L.info ("Linked to #{oap}")
+		end
+
+
+	
+
+	end #thread
+
+
+	sleep 1 while Thread.list.count>1 
+
+	exit
+
+end #iteration over all rows
+
+
